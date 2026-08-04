4fd9fcbe01bd6202f1569a6ace81d02ca8c0a39d
Happy Gao <happy.gao.dev@gmail.com>
2026-08-04 19:19:39 +0300

    settings for githubPages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/threejs-basics",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
